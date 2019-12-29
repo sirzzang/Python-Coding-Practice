7973fe5bd72ff4295f03b5c077755fac777e8642
sirzzang <sirzzang@naver.com>
2019-12-30 00:11:42 +0900

    백준 문제풀이 추가

---
+++ home_3053_수학2.py
@@ -0,0 +1,19 @@
+# math 모듈 없이 푸는 방법
+
+pi = 3.14159265359
+r = int(input())
+euclid = r*r*pi
+taxi = r*r*2
+
+print('%.6f' % euclid)
+print('%.6f' % taxi)
+
+# import math
+
+# r = int(input())
+# pi = math.pi
+# euclid_circle = r*r*pi
+# taxi_circle = 2*r*r
+
+# print('%.6f' % euclid_circle)
+# print('%.6f' % taxi_circle)
