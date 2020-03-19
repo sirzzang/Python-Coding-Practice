c17c23ad167d1a91ce1d2f52e704dd1070e23e83
sirzzang <sirzzang@naver.com>
2020-03-19 17:47:52 +0900

    20200319 백준 문제 풀이 : 강의실배정, 이분탐색 미완

--- 그리디/그리디_강의실배정_BOJ11000.py
+++ 그리디/그리디_강의실배정_BOJ11000.py
@@ -1,3 +1,37 @@
+import sys
+
+T = int(sys.stdin.readline())
+lectures = sorted([list(map(int, sys.stdin.readline().split())) for _ in range(T)], key = lambda x : x[0])
+# print(lectures)
+rooms = [0] * (T)
+# print(rooms)
+
+
+# lectures = [[0, 6], [1, 4], [2, 13], [3, 5], [3, 8], [5, 7], [5, 9], [6, 10], [8, 11], [8, 12], [12, 14]]# 디버그용
+# rooms = [0] * len(lectures)
+# print("강의 리스트 :", lectures) # 디버그용
+# print("강의실 번호 :", rooms) # 디버그용
+
+# 이러면 바뀌었던 애들도 계속 바뀐다!
+room_num = 1
+for i in range(len(lectures)):
+    if rooms[i] == 0:
+        rooms[i] = room_num
+        end_time = lectures[i][1]
+        for j in range(i+1, len(lectures)):
+            if lectures[j][0] >= end_time and rooms[j]==0: 
+                rooms[j] = room_num
+                end_time = lectures[j][1]        
+        room_num += 1
+print(room_num-1)
+
+# print("최종 강의실 번호 :", rooms) # 디버그용
+
+    
+    
+
+
+
 # 최소의 강의실을 사용해야 한다.
 # 1 3 - 3 5 이렇게 이어지는 애들은 한 강의실에서 할 수 있으니까 cnt += 1하면 안 된다.
 # 회의실 문제 변형하면, 끝 시간으로 쭉 정렬한 애가 한 강의실에서 할 수 있는 애들.
