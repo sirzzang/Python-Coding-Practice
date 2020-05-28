41851499f4542e71eaaefdf1c756e6906ee33214
sirzzang <sirzzang@naver.com>
2020-05-29 02:05:05 +0900

    문제풀이 추가

---
+++ BOJ/BOJ1120_문자열.py
@@ -0,0 +1,15 @@
+a, b = input().split()
+answer = 50 # 초기값 설정
+
+for i in range(len(b)-len(a)+1):
+    flag = b[i:i+len(a)]
+    print(f"======== Checking '{a}' in '{flag}' ========")
+    diff_cnt = 0
+    for j in range(len(flag)):
+        if a[j] != flag[j]:
+            diff_cnt += 1
+    print(f"Different Letters Cnt : {diff_cnt}")
+    if diff_cnt < answer:
+        answer = diff_cnt
+
+print(answer)
