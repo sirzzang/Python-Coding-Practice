9c97e1cd1d88f125c2a22896bd54a2845d793b22
sirzzang <sirzzang@naver.com>
2020-09-05 19:04:42 +0900

    20200905 문제풀이 이어서 필요

--- PROGRAMMERS/Programmers_짝지어제거하기.py
+++ PROGRAMMERS/Programmers_짝지어제거하기.py
@@ -1,4 +1,45 @@
-import re
+# 2차 시도
+'''
+정확성  테스트
+테스트 1 〉	통과 (0.01ms, 9.48MB)
+테스트 2 〉	통과 (6.98ms, 10.8MB)
+테스트 3 〉	실패 (런타임 에러)
+테스트 4 〉	실패 (런타임 에러)
+테스트 5 〉	실패 (런타임 에러)
+테스트 6 〉	실패 (런타임 에러)
+테스트 7 〉	실패 (런타임 에러)
+테스트 8 〉	실패 (런타임 에러)
+테스트 9 〉	통과 (0.00ms, 9.51MB)
+테스트 10 〉	통과 (0.09ms, 9.52MB)
+테스트 11 〉	통과 (0.01ms, 9.65MB)
+테스트 12 〉	통과 (0.01ms, 9.7MB)
+테스트 13 〉	통과 (0.01ms, 9.54MB)
+효율성  테스트
+테스트 1 〉	실패 (시간 초과)
+테스트 2 〉	통과 (64.58ms, 22.4MB)
+테스트 3 〉	실패 (런타임 에러)
+테스트 4 〉	실패 (런타임 에러)
+테스트 5 〉	실패 (런타임 에러)
+테스트 6 〉	실패 (런타임 에러)
+테스트 7 〉	실패 (런타임 에러)
+테스트 8 〉	실패 (런타임 에러)
+'''
+
+
+def solution(s):
+    if len(s) == 0:
+        return 1
+    elif len(s) == 1:
+        return 0
+    
+    duplicates = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg', 'hh', 'ii', 'jj', 'kk', 'll', 'mm',
+                 'nn', 'oo', 'pp', 'qq', 'rr', 'ss', 'tt', 'uu', 'vv', 'ww', 'xx', 'yy', 'zz']
+    for d in duplicates:
+        if d in s:
+            s = s.replace(d, '')
+            return solution(s)
+    else:
+        return 0
 
 
 # 1차 시도
@@ -27,6 +68,8 @@ import re
 테스트 7 〉	실패 (1.72ms, 13.9MB)
 테스트 8 〉	실패 (1.73ms, 13.8MB)
 '''
+
+import re
 
 def is_duplicate(s, pattern):
     if re.search(pattern, s) is None:
